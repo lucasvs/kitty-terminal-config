6ee148a5efbad3dee2c8b4ddb7278ee050225260
Lucas Souza <lucas@fluxoti.com>
2024-09-16 23:55:25 -0300

    Fix ultrawide monitor resolution

--- startup.sh
+++ startup.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 kitty @ launch --location=vsplit
 kitty @resize-window -m recent:0 --axis vertical -i 3
-kitty @resize-window -m recent:0 --axis horizontal -i 30
+kitty @resize-window -m recent:0 --axis horizontal -i 40
